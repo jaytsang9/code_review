9d53601670fadff6610b02718069b16947f24a53
Johnny Tsang <jaytsang9@gmail.com>
2021-03-21 14:16:52 -0400

    set up the index view

---
+++ reviews/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class review(models.Model):
+    title = models.CharField(max_length=64)
+    author = models.CharField(max_length=64)
+    code = models.TextField()
+    lang_type = models.CharField(
+        max_length = 12,
+        choices=[
+            ('PY', 'Python'),
+            ('JS', 'JavaScript'),
+            ('PHP', 'PHP'),
+            ('JAVA', 'Java'),
+            ('CPP', 'C++'),
+            ('C', 'C'),
+            ('CSHARP', 'C#'),
+            ('R', 'R'),
+            ('RBY', 'Ruby'),
+            ('GO', 'Go'),
+            ('SWIFT', 'Swift'),
+            ('SQL', 'SQL')
+        ]
+    )
+    votes = models.IntegerField()
+    date = models.DateField()
