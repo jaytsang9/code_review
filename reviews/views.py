668c4f42fa8fc1d28744e81d90f042bb0f8f2f61
Johnny Tsang <jaytsang9@gmail.com>
2021-04-09 20:36:46 -0400

    add users app

--- reviews/views.py
+++ reviews/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Review
 
 # Create your views here.
-def index(request):
+def reviews(request):
     reviews = Review.objects.all()
     context = {'reviews': reviews}
     return render(request, 'reviews/reviews.html', context)
