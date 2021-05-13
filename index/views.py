92aa17a49131d5201af1fe18ca67b1ac2134dfdb
Johnny Tsang <jaytsang9@gmail.com>
2021-05-12 23:33:35 -0400

    add OAuth

--- index/views.py
+++ index/views.py
@@ -22,14 +22,73 @@ def create(request):
     return render(request, 'index/create.html', {'form':form})
 
 def py_reviews(request):
-    reviews = Review.objects.filter(language='Python')[0]
+    reviews = Review.objects.filter(language='Python')
     data = {
-        "title": reviews.title,
-        "user": reviews.user,
-        "language": reviews.language,
+        "title": reviews[0].title,
+        "user": reviews[0].user,
+        "language": reviews[0].language,
     }
-
     return JsonResponse(data)
+
+# def js_reviews(request):
+#     reviews = Review.objects.filter(language='JavaScript')[0]
+#     data = {
+#         "title": reviews.title,
+#         "user": reviews.user,
+#         "language": reviews.language,
+#     }
+
+#     return JsonResponse(data)
+
+# def php_reviews(request):
+#     reviews = Review.objects.filter(language='PHP')[0]
+#     data = {
+#         "title": reviews.title,
+#         "user": reviews.user,
+#         "language": reviews.language,
+#     }
+
+#     return JsonResponse(data)
+
+# def java_reviews(request):
+#     reviews = Review.objects.filter(language='Java')[0]
+#     data = {
+#         "title": reviews.title,
+#         "user": reviews.user,
+#         "language": reviews.language,
+#     }
+
+#     return JsonResponse(data)
+
+# def cpp_reviews(request):
+#     reviews = Review.objects.filter(language='C++')[0]
+#     data = {
+#         "title": reviews.title,
+#         "user": reviews.user,
+#         "language": reviews.language,
+#     }
+
+#     return JsonResponse(data)
+
+# def c_reviews(request):
+#     reviews = Review.objects.filter(language='C')[0]
+#     data = {
+#         "title": reviews.title,
+#         "user": reviews.user,
+#         "language": reviews.language,
+#     }
+
+#     return JsonResponse(data)
+
+# def sql_reviews(request):
+#     reviews = Review.objects.filter(language='SQL')[0]
+#     data = {
+#         "title": reviews.title,
+#         "user": reviews.user,
+#         "language": reviews.language,
+#     }
+
+#     return JsonResponse(data)
 
 def show_review(request, num):
     review = Review.objects.filter(id=num)
